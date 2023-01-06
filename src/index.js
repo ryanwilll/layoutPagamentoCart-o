e16798ce0c122d2430fefc30f248d4ee71d13265
Ryan Will Daros <ryan_will-@hotmail.com>
2023-01-06 11:01:40 -0300

    Modificação [1] JS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ document.querySelector("#nome-cartao").oninvalid = function (evt) {
   // essa linha cancela o comportamento padrão do navegador
   evt.preventDefault();
   // faz a validação novamente
+
   if (!this.validity.valid) {
     //aqui eu mostro o alert
-    alert("O campo nome é obrigatório!");
   }
 };
 
@@ -23,6 +23,23 @@ function lettersOnly(evt) {
     (charCode < 97 || charCode > 122)
   ) {
     alert("Apenas letras");
+
+    return false;
+  }
+  return true;
+}
+function numberOnly(evt) {
+  evt = evt ? evt : event;
+
+  var charCode = evt.charCode
+    ? evt.charCode
+    : evt.keyCode
+    ? evt.keyCode
+    : evt.which
+    ? evt.which
+    : 0;
+  if (charCode < 48 || charCode > 57) {
+    alert("Numeros");
 
     return false;
   }
